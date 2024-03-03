dedaa84d0adcae9630cf8ece18ee23f114ab794a
Fabio Mariani <33620930+FabioMariani@users.noreply.github.com>
2024-03-03 16:16:20 +0100

    rubric update

--- assets/statuti.js
+++ assets/statuti.js
@@ -1,7 +1,7 @@
-const volumeSelect = document.getElementById('volume-select');
-const bookSelect = document.getElementById('book-select');
-const statuteSelect = document.getElementById('statute-select');
-const contentElement = document.getElementById('content');
+volumeSelect = document.getElementById('volume-select');
+bookSelect = document.getElementById('book-select');
+rubricSelect = document.getElementById('rubric-select');
+contentElement = document.getElementById('content');
 
 fetch('assets/statuti_web.json')
     .then(response => response.json())
@@ -10,71 +10,75 @@ fetch('assets/statuti_web.json')
         function generateNavigation() {
             volumes = Object.keys(data);
             volumeSelect.addEventListener('change', handleVolumeChange);
-            volumes.forEach(volume => {
-                const option = document.createElement('option');
-                option.value = volume;
-                option.textContent = volume;
+            for (v_i in volumes) {
+                option = document.createElement('option');
+                option.value = v_i;
+                option.textContent = volumes[v_i];
                 volumeSelect.appendChild(option);
-            });
+            }
         }
 
         // Function to populate books and content
         function populateBooksAndContent(selectedVolume) {
-            volume_selected = data[selectedVolume];
+            volume_selected_key = Object.keys(data)[selectedVolume]
+            volume_selected = data[volume_selected_key];
             if (typeof volume_selected === "string") {
                 updateURLParams(selectedVolume, null, null); // Update URL params
                 contentElement.innerHTML = volume_selected;
             } else {
-                const books = Object.keys(volume_selected);
-                books.forEach(book => {
-                    const option = document.createElement('option');
-                    option.value = book;
-                    option.textContent = book;
+                books = Object.keys(volume_selected);
+                for (b_i in books) {
+                    option = document.createElement('option');
+                    option.value = b_i;
+                    option.textContent = books[b_i];
                     bookSelect.appendChild(option);
-                });
+                }
             }
             // Trigger book change event if books are available
             if (bookSelect.options.length > 0) {
                 bookSelect.selectedIndex = 0; // Select the first book by default
-                const selectedBook = bookSelect.value;
+                selectedBook = bookSelect.value;
                 updateURLParams(selectedVolume, selectedBook, null); // Update URL params
-                populateStatutesAndContent(selectedVolume, selectedBook);
+                populateRubricsAndContent(selectedVolume, selectedBook);
             }
         }
 
-        // Function to populate statutes and content
-        function populateStatutesAndContent(selectedVolume, selectedBook) {
-            const book_selected = data[selectedVolume][selectedBook];
+        // Function to populate rubrics and content
+        function populateRubricsAndContent(selectedVolume, selectedBook) {
+            volume_selected_key = Object.keys(data)[selectedVolume]
+            book_selected_key = Object.keys(data[volume_selected_key])[selectedBook]
+            book_selected = data[volume_selected_key][book_selected_key];
             if (typeof book_selected === "string") {
                 updateURLParams(selectedVolume, selectedBook, null); // Update URL params
                 contentElement.innerHTML = book_selected;
             } else {
-                const statutes = Object.keys(book_selected);
-                statutes.forEach(statute => {
-                    const option = document.createElement('option');
-                    option.value = statute;
-                    option.textContent = statute;
-                    statuteSelect.appendChild(option);
-                });
+                rubrics = Object.keys(book_selected);
+                for (r_i in rubrics) {
+                    option = document.createElement('option');
+                    option.value = r_i;
+                    option.textContent = rubrics[r_i];
+                    rubricSelect.appendChild(option);
+                }
             }
-            if (statuteSelect.options.length > 0) {
-                statuteSelect.selectedIndex = 0; // Select the first statute by default
-                const selectedStatute = statuteSelect.value;
-                updateURLParams(selectedVolume, selectedBook, selectedStatute); // Update URL params
-                const statuteText = data[selectedVolume][selectedBook][selectedStatute];
-                contentElement.innerHTML = statuteText;
+            if (rubricSelect.options.length > 0) {
+                rubricSelect.selectedIndex = 0; // Select the first rubric by default
+                selectedRubric = rubricSelect.value;
+                updateURLParams(selectedVolume, selectedBook, selectedRubric); // Update URL params
+                rubric_key = Object.keys(data[volume_selected_key][book_selected_key])[selectedRubric];
+                rubricText = data[volume_selected_key][book_selected_key][rubric_key]
+                contentElement.innerHTML = rubricText;
             }
         }
 
-        // Function to programmatically select volume, book, and statute
-        function selectDocument(volume, book, statute) {
+        // Function to programmatically select volume, book, and rubric
+        function selectDocument(volume, book, rubric) {
             volumeSelect.value = volume;
             handleVolumeChange({ target: volumeSelect });
-            if (book && statute) {
+            if (book && rubric) {
                 bookSelect.value = book;
                 handleBookChange({ target: bookSelect });
-                statuteSelect.value = statute;
-                handleStatuteChange({ target: statuteSelect });
+                rubricSelect.value = rubric;
+                handleRubricChange({ target: rubricSelect });
             } else if (book) {
                 bookSelect.value = book;
                 handleBookChange({ target: bookSelect });
@@ -87,69 +91,87 @@ fetch('assets/statuti_web.json')
             updateURLParams(selectedVolume, null, null); // Update URL params
             contentElement.innerHTML = ''; // Clear previous content
             bookSelect.innerHTML = '';
-            statuteSelect.innerHTML = '';
+            rubricSelect.innerHTML = '';
             populateBooksAndContent(selectedVolume);
         }
 
         // Function to handle book change event
         function handleBookChange(event) {
-            const selectedVolume = volumeSelect.value;
-            const selectedBook = event.target.value;
+            selectedVolume = volumeSelect.value;
+            selectedBook = event.target.value;
             updateURLParams(selectedVolume, selectedBook, null); // Update URL params
             contentElement.innerHTML = ''; // Clear previous content
-            statuteSelect.innerHTML = '';
-            populateStatutesAndContent(selectedVolume, selectedBook);
+            rubricSelect.innerHTML = '';
+            populateRubricsAndContent(selectedVolume, selectedBook);
         }
 
-        // Function to handle statute change event
-        function handleStatuteChange(event) {
-            const selectedVolume = volumeSelect.value;
-            const selectedBook = bookSelect.value;
-            const selectedStatute = event.target.value;
-            updateURLParams(selectedVolume, selectedBook, selectedStatute); // Update URL params
-            const statuteText = data[selectedVolume][selectedBook][selectedStatute];
-            contentElement.innerHTML = statuteText;
+        // Function to handle rubric change event
+        function handleRubricChange(event) {
+            selectedVolume = volumeSelect.value;
+            selectedBook = bookSelect.value;
+            selectedRubric = event.target.value;
+            updateURLParams(selectedVolume, selectedBook, selectedRubric); // Update URL params
+            volume_selected_key = Object.keys(data)[selectedVolume]
+            book_selected_key = Object.keys(data[volume_selected_key])[selectedBook]
+            rubric_key = Object.keys(data[volume_selected_key][book_selected_key])[selectedRubric];
+            rubricText = data[volume_selected_key][book_selected_key][rubric_key]
+            contentElement.innerHTML = rubricText;
         }
 
         // Function to update URL parameters
         function updateURLParams(volume, book, statute) {
-            console.log(volume, book, statute)
-            const params = new URLSearchParams(window.location.search);
-            params.delete('s'); // Clear existing volume param
-            params.delete('l'); // Clear existing book param
-            params.delete('r'); // Clear existing statute param
-            params.set('s', volume);
-            if (book) params.set('l', book);
-            if (statute) params.set('r', statute);
-            window.history.replaceState({}, '', `${location.pathname}?${params}`);
+            console.log(volume, book, statute);
+
+            let url = '/';
+            if (volume) url += volume + '/';
+            if (book) url += book + '/';
+            if (statute) url += statute + '/';
+
+            // Rimuovi l'ultimo '/' se presente
+            if (url.endsWith('/')) {
+                url = url.slice(0, -1);
+            }
+
+            window.history.replaceState({}, '', url);
         }
 
         // Function to get URL parameters
         function getURLParams() {
-            const params = new URLSearchParams(window.location.search);
-            const volume = params.get('s');
-            const book = params.get('l');
-            const statute = params.get('r');
-            return { volume, book, statute };
+            pathSegments = window.location.pathname.split('/').filter(segment => segment !== '');
+            let volume = null;
+            let book = null;
+            let rubric = null;
+
+            if (pathSegments.length >= 1) {
+                volume = pathSegments[0];
+            }
+            if (pathSegments.length >= 2) {
+                book = pathSegments[1];
+            }
+            if (pathSegments.length >= 3) {
+                rubric = pathSegments[2];
+            }
+
+            return { volume, book, rubric };
         }
 
-        // Attach event listeners for book and statute changes
+        // Attach event listeners for book and rubric changes
         bookSelect.addEventListener('change', handleBookChange);
-        statuteSelect.addEventListener('change', handleStatuteChange);
+        rubricSelect.addEventListener('change', handleRubricChange);
 
         // Generate initial navigation
         generateNavigation();
 
         // Check URL parameters and update selections accordingly
-        const { volume, book, statute } = getURLParams();
+        var { volume, book, rubric } = getURLParams();
         if (volume) {
             try{
-                selectDocument(volume, book, statute);
+                selectDocument(volume, book, rubric);
             }
             catch{
-                selectDocument('Statuti del Comune', 'Introduzione');
+                selectDocument(0);
             }
         } else{
-                selectDocument('Statuti del Comune', 'Introduzione');
+                selectDocument(0);
         }
     });
