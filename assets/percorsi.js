f2835ce2b84816e243c319533067227417cc541c
Fabio Mariani <33620930+FabioMariani@users.noreply.github.com>
2024-03-23 11:52:34 +0100

    update_map

--- assets/percorsi.js
+++ assets/percorsi.js
@@ -29,7 +29,7 @@ fetch('assets/statuti_web.json').then(response => response.json()).then(data => 
    });
 
 
-   fetch('assets/comune.json').then(response => response.json()).then(geoJSONdata => {
+   fetch('assets/comune.geojson').then(response => response.json()).then(geoJSONdata => {
         //city map
         var cityMapBounds = [[42.84455370395206, 13.556849956512453], [42.86343017090419,13.593156337738039]];
 
@@ -63,6 +63,7 @@ fetch('assets/statuti_web.json').then(response => response.json()).then(data => 
             switch (variable) {
                 case "porta":
                     return "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d=\"M336.6 156.5c1.3 1.1 2.7 2.2 3.9 3.3c9.3 8.2 23 10.5 33.4 3.6l67.6-45.1c11.4-7.6 14.2-23.2 5.1-33.4C430 66.6 410.9 50.6 389.7 37.6c-11.9-7.3-26.9-1.4-32.1 11.6l-30.5 76.2c-4.5 11.1 .2 23.6 9.5 31.2zM328 36.8c5.1-12.8-1.6-27.4-15-30.5C294.7 2.2 275.6 0 256 0s-38.7 2.2-57 6.4C185.5 9.4 178.8 24 184 36.8l30.3 75.8c4.5 11.3 16.8 17.2 29 16c4.2-.4 8.4-.6 12.7-.6s8.6 .2 12.7 .6c12.1 1.2 24.4-4.7 29-16L328 36.8zM65.5 85c-9.1 10.2-6.3 25.8 5.1 33.4l67.6 45.1c10.3 6.9 24.1 4.6 33.4-3.6c1.3-1.1 2.6-2.3 4-3.3c9.3-7.5 13.9-20.1 9.5-31.2L154.4 49.2c-5.2-12.9-20.3-18.8-32.1-11.6C101.1 50.6 82 66.6 65.5 85zm314 137.1c.9 3.3 1.7 6.6 2.3 10c2.5 13 13 23.9 26.2 23.9h80c13.3 0 24.1-10.8 22.9-24c-2.5-27.2-9.3-53.2-19.7-77.3c-5.5-12.9-21.4-16.6-33.1-8.9l-68.6 45.7c-9.8 6.5-13.2 19.2-10 30.5zM53.9 145.8c-11.6-7.8-27.6-4-33.1 8.9C10.4 178.8 3.6 204.8 1.1 232c-1.2 13.2 9.6 24 22.9 24h80c13.3 0 23.8-10.8 26.2-23.9c.6-3.4 1.4-6.7 2.3-10c3.1-11.4-.2-24-10-30.5L53.9 145.8zM104 288H24c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h80c13.3 0 24-10.7 24-24V312c0-13.3-10.7-24-24-24zm304 0c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h80c13.3 0 24-10.7 24-24V312c0-13.3-10.7-24-24-24H408zM24 416c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h80c13.3 0 24-10.7 24-24V440c0-13.3-10.7-24-24-24H24zm384 0c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h80c13.3 0 24-10.7 24-24V440c0-13.3-10.7-24-24-24H408zM272 192c0-8.8-7.2-16-16-16s-16 7.2-16 16V464c0 8.8 7.2 16 16 16s16-7.2 16-16V192zm-64 32c0-8.8-7.2-16-16-16s-16 7.2-16 16V464c0 8.8 7.2 16 16 16s16-7.2 16-16V224zm128 0c0-8.8-7.2-16-16-16s-16 7.2-16 16V464c0 8.8 7.2 16 16 16s16-7.2 16-16V224z\"/></svg>";
+                case "altro":
                 case "piazza":
                     return "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d=\"M320 64A64 64 0 1 0 192 64a64 64 0 1 0 128 0zm-96 96c-35.3 0-64 28.7-64 64v48c0 17.7 14.3 32 32 32h1.8l11.1 99.5c1.8 16.2 15.5 28.5 31.8 28.5h38.7c16.3 0 30-12.3 31.8-28.5L318.2 304H320c17.7 0 32-14.3 32-32V224c0-35.3-28.7-64-64-64H224zM132.3 394.2c13-2.4 21.7-14.9 19.3-27.9s-14.9-21.7-27.9-19.3c-32.4 5.9-60.9 14.2-82 24.8c-10.5 5.3-20.3 11.7-27.8 19.6C6.4 399.5 0 410.5 0 424c0 21.4 15.5 36.1 29.1 45c14.7 9.6 34.3 17.3 56.4 23.4C130.2 504.7 190.4 512 256 512s125.8-7.3 170.4-19.6c22.1-6.1 41.8-13.8 56.4-23.4c13.7-8.9 29.1-23.6 29.1-45c0-13.5-6.4-24.5-14-32.6c-7.5-7.9-17.3-14.3-27.8-19.6c-21-10.6-49.5-18.9-82-24.8c-13-2.4-25.5 6.3-27.9 19.3s6.3 25.5 19.3 27.9c30.2 5.5 53.7 12.8 69 20.5c3.2 1.6 5.8 3.1 7.9 4.5c3.6 2.4 3.6 7.2 0 9.6c-8.8 5.7-23.1 11.8-43 17.3C374.3 457 318.5 464 256 464s-118.3-7-157.7-17.9c-19.9-5.5-34.2-11.6-43-17.3c-3.6-2.4-3.6-7.2 0-9.6c2.1-1.4 4.8-2.9 7.9-4.5c15.3-7.7 38.8-14.9 69-20.5z\"/></svg>";
                 case "chiesa":
@@ -92,8 +93,8 @@ fetch('assets/statuti_web.json').then(response => response.json()).then(data => 
                 fillOpacity: 0.7,
             });
         }
-
-        var sestieriLayer = L.geoJSON(geoJSONdata.sestieri, {
+        sestieri = geoJSONdata.features.slice(0, 24);
+        var sestieriLayer = L.geoJSON(sestieri, {
             onEachFeature: function(feature, layer) {
                 layer.on('click', function(e) {
                     // Check if the clicked layer is different from the currently selected layer
@@ -104,7 +105,7 @@ fetch('assets/statuti_web.json').then(response => response.json()).then(data => 
                             selectedLayer.bringToBack();
                         }
                         selectedLayer = layer; // Update the selected layer
-                        populateResults(feature.properties.id, feature.properties.id, "city-results", cityCurrentPage, cityResults)
+                        populateResults(feature.properties.id, feature.properties.title, "city-results", cityCurrentPage, cityResults)
                         selectSestiere(layer)
                         e.target.bringToFront();
                     }
@@ -114,7 +115,7 @@ fetch('assets/statuti_web.json').then(response => response.json()).then(data => 
                         selectedMarker = null;
                     }
                 });
-                layer.bindTooltip(feature.properties.id);
+                layer.bindTooltip(feature.properties.title);
                 layer.on('mouseover', function(e) {
                     // Apply hover style only if the layer is not selected
                     if (layer !== selectedLayer) {
@@ -143,8 +144,8 @@ fetch('assets/statuti_web.json').then(response => response.json()).then(data => 
             }
         }).addTo(cityMap);
 
-
-        var cityMapMarkersLayer = L.geoJSON(geoJSONdata.cityMarkers, {
+        cityMarkers = geoJSONdata.features.slice(24);
+        var cityMapMarkersLayer = L.geoJSON(cityMarkers, {
              pointToLayer: function (feature, latlng) {
                     return L.marker(latlng, {
                         icon: L.divIcon({
@@ -161,7 +162,7 @@ fetch('assets/statuti_web.json').then(response => response.json()).then(data => 
                             selectedMarker = null;
                         }
                         selectedMarker = layer;
-                        populateResults(feature.properties.id, feature.properties.id, "city-results", cityCurrentPage, cityResults)
+                        populateResults(feature.properties.id, feature.properties.title, "city-results", cityCurrentPage, cityResults)
                         L.DomUtil.addClass(selectedMarker._icon, 'selectedMarker');
                         // Deselect the polygon if it's selected
                         if (selectedLayer) {
@@ -171,7 +172,7 @@ fetch('assets/statuti_web.json').then(response => response.json()).then(data => 
                         }
                     }
                 });
-                layer.bindTooltip(feature.properties.id);
+                layer.bindTooltip(feature.properties.title);
              }
         }).addTo(cityMap);
 
@@ -238,11 +239,11 @@ fetch('assets/statuti_web.json').then(response => response.json()).then(data => 
                             selectedRegionMarker = null;
                         }
                         selectedRegionMarker = layer;
-                        populateResults(feature.properties.id, feature.properties.id, "region-results", regionCurrentPage, regionResults)
+                        populateResults(feature.properties.id, feature.properties.title, "region-results", regionCurrentPage, regionResults)
                         L.DomUtil.addClass(selectedRegionMarker._icon, 'selectedMarker');
                     }
                 });
-                layer.bindTooltip(feature.properties.id);
+                layer.bindTooltip(feature.properties.title);
              }
         }).addTo(regionMap);
 
