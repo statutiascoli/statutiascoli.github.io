ef7b5bd9d845bd53b540ea0508f406c6a3d0268d
Fabio Mariani <33620930+FabioMariani@users.noreply.github.com>
2024-04-16 08:13:35 +0200

    update

--- assets/statuti.js
+++ assets/statuti.js
@@ -3,9 +3,59 @@ bookSelect = document.getElementById('book-select');
 rubricSelect = document.getElementById('rubric-select');
 contentElement = document.getElementById('content');
 
-function fillContent(contentText){
+const urlString = window.location.href;
+const url = new URL(urlString);
+const domain = url.hostname;
+
+function decrypt(data, key) {
+    let decryptedData = '';
+    let keyLength = key.length;
+    for (let i = 0; i < data.length; i++) {
+        decryptedData += String.fromCharCode(data.charCodeAt(i) ^ key.charCodeAt(i % keyLength));
+    }
+    return decryptedData;
+}
+
+
+async function fillContent(contentText){
     let contentDiv = document.createElement('div');
     contentDiv.innerHTML = contentText;
+    contentDiv = contentDiv.firstChild
+    let facsList = contentDiv.getAttribute("facs").split(" ")
+    let facsDiv = document.createElement('div');
+    facsDiv.classList.add('row', 'row-cols-1', 'row-cols-md-4', 'g-4');
+    for (let fIm of facsList){
+        let imgTit = fIm.split(".jpg")[0]
+        let imgLabel
+        if (imgTit.includes("r")){
+            imgLabel = "Carta " + imgTit.split("r")[0] + " Recto"
+        }
+        if (imgTit.includes("v")){
+            imgLabel = "Carta " + imgTit.split("v")[0] + " Verso"
+        }
+        let imgPath = "assets/path/" + imgTit
+        let response = await fetch(imgPath);
+        let data = await response.text();
+        let decryptedImageSrc = decrypt(data, domain);
+        let outerDiv = document.createElement('div');
+        outerDiv.classList.add('col');
+        let innerDiv = document.createElement('div');
+        innerDiv.classList.add('card', 'h-100');
+        let img = document.createElement('img');
+        img.classList.add('card-img-top');
+        img.src = "data:image/png;base64, " + decryptedImageSrc;
+        let cardBodyDiv = document.createElement('div');
+        cardBodyDiv.classList.add('card-body');
+        let heading = document.createElement('h5');
+        heading.classList.add('card-title');
+        heading.textContent = imgLabel; // Set the text content of the heading
+        cardBodyDiv.appendChild(heading); // Append heading to card body
+        innerDiv.appendChild(img); // Append image to inner div
+        innerDiv.appendChild(cardBodyDiv); // Append card body to inner div
+        outerDiv.appendChild(innerDiv); // Append inner div to outer div
+        facsDiv.appendChild(outerDiv)
+    }
+
     let noteElement = contentDiv.querySelector('div.summary');
     let abstractDiv = null
     if (noteElement) {
@@ -31,6 +81,8 @@ function fillContent(contentText){
     </div>`
     contentElement.innerHTML = elementStat;
     document.getElementById('transcription').appendChild(contentDiv)
+        document.getElementById('digital').appendChild(facsDiv)
+
     if (noteElement){
         document.getElementById('abstract').appendChild(noteElement)
     }
@@ -58,10 +110,10 @@ fetch('assets/statuti_web.json')
         }
 
         // Function to populate books and content
-        function populateBooksAndContent(selectedVolume) {
+        function populateBooksAndContent(selectedVolume, addContent) {
             volume_selected_key = Object.keys(data)[selectedVolume]
             volume_selected = data[volume_selected_key];
-            if (typeof volume_selected === "string") {
+            if (typeof volume_selected === "string" && addContent) {
                 updateURLParams(selectedVolume, null, null); // Update URL params
                 fillContent(volume_selected)
             } else {
@@ -74,7 +126,7 @@ fetch('assets/statuti_web.json')
                 }
             }
             // Trigger book change event if books are available
-            if (bookSelect.options.length > 0) {
+            if (bookSelect.options.length > 0 && addContent) {
                 bookSelect.selectedIndex = 0; // Select the first book by default
                 selectedBook = bookSelect.value;
                 updateURLParams(selectedVolume, selectedBook, null); // Update URL params
@@ -83,11 +135,11 @@ fetch('assets/statuti_web.json')
         }
 
         // Function to populate rubrics and content
-        function populateRubricsAndContent(selectedVolume, selectedBook) {
+        function populateRubricsAndContent(selectedVolume, selectedBook, addContent) {
             volume_selected_key = Object.keys(data)[selectedVolume]
             book_selected_key = Object.keys(data[volume_selected_key])[selectedBook]
             book_selected = data[volume_selected_key][book_selected_key];
-            if (typeof book_selected === "string") {
+            if (typeof book_selected === "string" && addContent) {
                 updateURLParams(selectedVolume, selectedBook, null); // Update URL params
                 fillContent(book_selected)
             } else {
@@ -99,7 +151,7 @@ fetch('assets/statuti_web.json')
                     rubricSelect.appendChild(option);
                 }
             }
-            if (rubricSelect.options.length > 0) {
+            if (rubricSelect.options.length > 0 && addContent) {
                 rubricSelect.selectedIndex = 0; // Select the first rubric by default
                 selectedRubric = rubricSelect.value;
                 updateURLParams(selectedVolume, selectedBook, selectedRubric); // Update URL params
@@ -115,57 +167,67 @@ fetch('assets/statuti_web.json')
         // Function to programmatically select volume, book, and rubric
         function selectDocument(volume, book, rubric) {
             volumeSelect.value = volume;
-            handleVolumeChange({ target: volumeSelect });
+            handleVolumeChange({ target: volumeSelect }, addContent=false);
             if (book && rubric) {
                 bookSelect.value = book;
-                handleBookChange({ target: bookSelect });
+                handleBookChange({ target: bookSelect }, addContent=false);
                 rubricSelect.value = rubric;
-                handleRubricChange({ target: rubricSelect });
+                handleRubricChange({ target: rubricSelect }, addContent=false);
             } else if (book) {
                 bookSelect.value = book;
+                handleBookChange({ target: bookSelect }, addContent=false);
+            }
+            if (rubric){
+                handleRubricChange({ target: rubricSelect });
+            }
+            else if (book) {
                 handleBookChange({ target: bookSelect });
+            }
+            else {
+                handleVolumeChange({ target: volumeSelect });
             }
         }
 
         // Function to handle volume change event
-        function handleVolumeChange(event) {
+        function handleVolumeChange(event, addContent=true) {
             selectedVolume = event.target.value;
             updateURLParams(selectedVolume, null, null); // Update URL params
             contentElement.innerHTML = ''; // Clear previous content
             bookSelect.innerHTML = '';
             rubricSelect.innerHTML = '';
-            populateBooksAndContent(selectedVolume);
+            populateBooksAndContent(selectedVolume, addContent);
         }
 
         // Function to handle book change event
-        function handleBookChange(event) {
+        function handleBookChange(event, addContent=true) {
             selectedVolume = volumeSelect.value;
             selectedBook = event.target.value;
             updateURLParams(selectedVolume, selectedBook, null); // Update URL params
             contentElement.innerHTML = ''; // Clear previous content
             rubricSelect.innerHTML = '';
-            populateRubricsAndContent(selectedVolume, selectedBook);
+            populateRubricsAndContent(selectedVolume, selectedBook, addContent);
         }
 
         // Function to handle rubric change event
-        function handleRubricChange(event) {
+        function handleRubricChange(event, addContent=true) {
             selectedVolume = volumeSelect.value;
             selectedBook = bookSelect.value;
             selectedRubric = event.target.value;
             updateURLParams(selectedVolume, selectedBook, selectedRubric); // Update URL params
-            volume_selected_key = Object.keys(data)[selectedVolume]
-            book_selected_key = Object.keys(data[volume_selected_key])[selectedBook]
-            rubric_key = Object.keys(data[volume_selected_key][book_selected_key])[selectedRubric];
-            rubricText = data[volume_selected_key][book_selected_key][rubric_key]
-            fillContent(rubricText)
-            document.querySelectorAll(".linkRubrica").forEach(function(element) {
-                element.innerHTML = '<svg class="index_link" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>'
-            });
+            if (addContent){
+                volume_selected_key = Object.keys(data)[selectedVolume]
+                book_selected_key = Object.keys(data[volume_selected_key])[selectedBook]
+                rubric_key = Object.keys(data[volume_selected_key][book_selected_key])[selectedRubric];
+                rubricText = data[volume_selected_key][book_selected_key][rubric_key]
+                fillContent(rubricText)
+                document.querySelectorAll(".linkRubrica").forEach(function(element) {
+                    element.innerHTML = '<svg class="index_link" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>'
+                });
+            }
         }
 
         // Function to update URL parameters
         function updateURLParams(volume, book, rubric) {
-            console.log(volume, book, rubric)
             params = new URLSearchParams(window.location.search);
             params.delete('id'); // Clear existing volume param
             id_p = ""
