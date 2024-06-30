fd939b33ec39ad4e6fbdef4cad917a371a544796
Fabio Mariani <33620930+FabioMariani@users.noreply.github.com>
2024-06-30 18:04:29 +0200

    Update statuti.js

--- assets/statuti.js
+++ assets/statuti.js
@@ -91,6 +91,9 @@ async function fillContent(contentText) {
         document.getElementById("abstract").remove();
         document.getElementById("abstractTab").remove();
     }
+    document.querySelectorAll(".linkRubrica").forEach(function(element) {
+        element.innerHTML = '<svg class="index_link" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>'
+    });
 }
 
 function getImageLabel(imgTit) {
@@ -170,9 +173,6 @@ fetch('assets/statuti_web.json')
                 rubric_key = Object.keys(data[volume_selected_key][book_selected_key])[selectedRubric];
                 rubricText = data[volume_selected_key][book_selected_key][rubric_key]
                 fillContent(rubricText)
-                document.querySelectorAll(".linkRubrica").forEach(function(element) {
-                    element.innerHTML = '<svg class="index_link" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>'
-                });
             }
         }
 
