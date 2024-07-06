fa5843d5a1ac0467aaa2971b8f8453576b686417
Fabio Mariani <33620930+FabioMariani@users.noreply.github.com>
2024-07-06 18:55:54 +0200

    Update percorsi.js

--- assets/percorsi.js
+++ assets/percorsi.js
@@ -38,7 +38,7 @@ fetch('assets/statuti_web.json').then(response => response.json()).then(data => 
             maxBoundsViscosity: 1.0,
             minZoom: 15,
             maxZoom: 17}).setView([42.854, 13.575], 15)
-        var cityMapimage = L.imageOverlay('assets/city.png', cityMapBounds).addTo(cityMap);
+        var cityMapimage = L.imageOverlay('assets/img.png', cityMapBounds).addTo(cityMap);
 
         cityMapCenter = cityMap.getBounds().getCenter();
         cityMap.setView(cityMapCenter, 15)
